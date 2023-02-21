d1da8037bd259fbabc85d40bb151279690dcd6ac
Albert <mona-m1@ya.ru>
2023-02-21 13:34:56 +0600

    mod task

--- HomeWork/task01.py
+++ HomeWork/task01.py
@@ -1,13 +1,17 @@
-def rifma(chant):
-    st = chant.lower().split()
+def rhyme(txt):
+    st = txt.lower().split()
     f = lambda x: sum(1 for i in x if i in 'аеёиоуыэюя')
     tmp = f(st[0])
     if all([f(i) == tmp for i in st]):
         return 'Парам пам-пам'
     return 'Пам парам'
 
+enter_user_rhyme=input("Enter your amazing rhyme")
+rhyme(enter_user_rhyme)
 
-print(rifma("Хорошо-живет-на-свете-Винни-Пух!\
- Оттого-поет-он-эти-Песни-вслух!"))
-
-print(rifma("пара-ра-рам рам-пам-папам па-ра-па-дам"))
+# print(Rhyme("Хорошо-живет-на-свете-Винни-Пух!\
+#  Оттого-поет-он-эти-Песни-вслух!"))
+#
+# print(Rhyme("пара-ра-рам рам-пам-папам па-ра-па-дам"))
+#
+# print(Rhyme("пара-ра-рам рам-пам-папам па-ра-па-дм"))
